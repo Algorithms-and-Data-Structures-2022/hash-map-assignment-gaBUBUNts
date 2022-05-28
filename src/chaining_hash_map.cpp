0171af335efd2f98cb4521aa8c1d18e0de6d9a20
gaBUBUNts <89221310+gaBUBUNts@users.noreply.github.com>
2022-05-28 21:04:09 +0300

    done HW

--- src/chaining_hash_map.cpp
+++ src/chaining_hash_map.cpp
@@ -39,17 +39,20 @@ namespace assignment {
     // 4. Увеличиваем кол-во ключей в словаре.
     // 5. Если превышен коэффициент загрузки словаря, то расширяем словарь.
 
-    // ... (ниже представлена часть реализации)
+    if (Contains(key)) {
+      return false;
+    }
 
     // вычисление индекса ячейки словаря по ключу
     const int index = hash(key, buckets_.size());
 
     // добавление пары "ключ-значение" в ячейку словаря (в конец связного списка)
     buckets_[index].push_back(Node(key, value));
+    num_keys_++;
 
     // расширение словаря до новой емкости в случае превышения коэффициента заполнения
     if (num_keys_ / static_cast<double>(buckets_.size()) > load_factor_) {
-      const int new_capacity = 0 /* здесь должна быть ваше выражение */;
+      const int new_capacity = static_cast<int>(buckets_.size()) * kGrowthCoefficient;
       resize(new_capacity);
     }
 
@@ -58,7 +61,7 @@ namespace assignment {
 
   std::optional<int> ChainingHashMap::Remove(int key) {
 
-    const int index = 0 /* напишите здесь свой код */;
+    const int index = hash(key, buckets_.size());
 
     // здесь используется итератор (по сути указатель на узел списка)
     for (auto it = buckets_[index].begin(); it != buckets_[index].end(); ++it) {
@@ -81,12 +84,14 @@ namespace assignment {
   std::optional<int> ChainingHashMap::Search(int key) const {
 
     // вычисление индекса ячейки для указанного ключа
-    const int index = 0 /* напишите здесь свой код */;
+    const int index = hash(key, buckets_.size());
 
     // Проходимся по всем элемента в ячейке словаря.
     // В худшем случае все элементы попали в одну ячейку словаря и сложность поиска ~ O(N).
     for (const Node& node : buckets_[index]) {
-      // напишите здесь свой код ...
+      if (node.key == key) {
+        return node.value;
+      }
     }
 
     return std::nullopt;
@@ -104,7 +109,13 @@ namespace assignment {
   }
 
   bool ChainingHashMap::Contains(int key) const {
-    // Напишите здесь свой код ...
+     std::list<Node> temp = buckets_[hash(key, buckets_.size())];
+     Node node = temp.front();
+     for (const Node node : temp) {
+       if (node.key == key) {
+         return true;
+       }
+    }
     return false;
   }
 
@@ -133,7 +144,7 @@ namespace assignment {
     // пересчитываем индексы элементов словаря, учитывая новую емкость
     for (const Bucket& bucket : buckets_) {
       for (const Node& node : bucket) {
-        const int new_index = 0 /* напишите здесь свой код */;
+        const int new_index = hash(node.key, new_capacity);
         new_buckets[new_index].push_back(node);
       }
     }
